b0ad944687e118dd204c88c5cd48030f4c64c126
pitcher0303 <pitcher0303@gmail.com>
2025-11-05 10:59:10 +0900

    첫 커밋

---
+++ src/main/java/com/pitchern/pitchernconfigserver/PitchernConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.pitchern.pitchernconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitchernConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PitchernConfigServerApplication.class, args);
+    }
+
+}
